9d96b14c3febec5d88fa2e7c9a9e7b4e575b2dce
amallad3 <106265499+amallad3@users.noreply.github.com>
2022-06-20 12:00:02 -0700

    Fix nearest neighbor algorithm

--- NearestNeighborHandler.java
+++ NearestNeighborHandler.java
@@ -27,38 +27,59 @@ public class NearestNeighborHandler implements Observer {
 	
 	private List<Line> nearestNeighbor(Board board) {
 		List<Line> lines = new LinkedList<Line>();
-		List<Point> points = new ArrayList<Point>(board.getPoints());
-		List<List<Integer>> visited = new ArrayList<List<Integer>>(points.size());
-
-		if (points.size() < 2) {
+		List<Point> points = new LinkedList<Point>(board.getPoints());
+		int total = points.size();
+		Boolean[] visited = new Boolean[total];
+		Queue<Integer> q = new LinkedList<Integer>();
+		
+		if (total < 2) {
 			return Collections.<Line>emptyList();
 		}
 		
-		for (int i = 0; i < points.size(); i++) {
-			visited.add(new ArrayList<Integer>());
-		}
-
-		for (int i = 0; i < points.size(); i++) {
-			
+		Point topLeft = new Point(0, 0, null);
+		int startIndex = 0;
+		{
 			float minDistance = Float.MAX_VALUE;
-			int minIndex = i;
+			for (int i = 0; i < total; i++) {
+				float distance = distanceTo(topLeft, points.get(i));
+				if (distance < minDistance) {
+					minDistance = distance;
+					startIndex = i;
+				}
+			}
+		}
+		
+		for (int i = 0; i < total; i++) {
+			visited[i] = false;
+		}
+		visited[startIndex] = true;
+		q.add(startIndex);
+		
+		int count = 0;
+		while (!q.isEmpty()) {
+			count++;
+			if (count == total) {
+				break;
+			}
+			Point startPoint = points.get(q.poll());
+			int endPointIndex = 0;
+			float minDistance = Float.MAX_VALUE;
 			
-			for (int j = 0; j < points.size(); j++) {
-				if (i != j) {
-					float distance = distanceTo(points.get(i), points.get(j));
+			for (int i = 0; i < total; i++) {
+				if (visited[i] == false) {
+					float distance = distanceTo(startPoint, points.get(i));
 					if (distance < minDistance) {
 						minDistance = distance;
-						minIndex = j;
+						endPointIndex = i;
 					}
 				}
 			}
 			
-			if (!visited.get(i).contains(minIndex)) {
-				visited.get(i).add(minIndex);
-				visited.get(minIndex).add(i);
-				lines.add(new Line(points.get(i), points.get(minIndex)));
-			}
+			visited[endPointIndex] = true;
+			q.add(endPointIndex);
+			lines.add(new Line(startPoint, points.get(endPointIndex)));
 		}
+		
 		return lines;
 	}
 	
