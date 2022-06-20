90f308040b39f42c1c41e30214dc031ed9fe1fcf
yzhu233 <86447424+yzhu233@users.noreply.github.com>
2022-06-20 16:25:25 -0700

    Update Point.java

--- Point.java
+++ Point.java
@@ -9,22 +9,46 @@ public class Point {
     private int y;
     private PointColor color;
 
-    public Point(int x, int y, PointColor color) {
+    private int clusterNumber;
+
+    public Point(int x, int y, PointColor color,int clusterNumber) {
         this.x = x;
         this.y = y;
         this.color = color;
+        this.clusterNumber=clusterNumber;
     }
 
     public int getX() {
         return this.x;
     }
-
+    
     public int getY() {
         return this.y;
     }
-
+    
     public PointColor getColor() {
         return this.color;
     }
+    
+    public int getClusterNumber() {
+        return clusterNumber;
+    }
+    
+    public void setClusterNumber(int i){
+        this.clusterNumber=clusterNumber;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public void setColor(PointColor color) {
+        this.color = color;
+    }
 }
+
 
